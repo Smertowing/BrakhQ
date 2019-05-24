3f57bb23247287c39825b4701c349c07e886c4f0
Kiryl <Kiryl.co@gmail.com>
2019-05-24 12:02:19 +0300

    .gitignore is now working

--- Pods/Target Support Files/DataCache/DataCache-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DataCache : NSObject
-@end
-@implementation PodsDummy_DataCache
-@end
--- Pods/Target Support Files/Pods-BrakhQ/Pods-BrakhQ-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BrakhQ : NSObject
-@end
-@implementation PodsDummy_Pods_BrakhQ
-@end
